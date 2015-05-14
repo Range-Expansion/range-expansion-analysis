4548c77cc7111a3763bc5e26ae41f81dc63490ad
btweinstein <btweinstein@gmail.com>
2015-05-14 18:32:19 -0400

    Still problems but taking a break. Trying to implement a more general way to write stuff to disk.

--- range_expansion_image.py
+++ range_expansion_image.py
@@ -27,7 +27,7 @@ class Multi_Experiment(object):
         self.hetero_r_list = [2.5, 3, 3.5, 4, 5, 6, 8, 10] # Radii used to compare heterozygosity
         self.num_theta_bins_list = [500, 600, 700, 800, 1000, 1000, 1500, 1500] # Bins at each radius; larger radii allow more bins
 
-    def write_nonlocal_quantity_to_disk(self, quantity, i= None, j=None):
+    def write_nonlocal_quantity_to_disk(self, quantity_str, i= None, j=None):
         for experiment, complete_im_sets in zip(self.experiment_list, self.complete_im_sets_list):
             quantity_list = []
             quantity_info = {}
@@ -37,18 +37,19 @@ class Multi_Experiment(object):
 
             # Make new directory for this experiment...give experiment a name
             for r, theta_bins in zip(self.hetero_r_list, self.num_theta_bins_list):
-                if quantity == 'hetero':
+                quantity = None
+                if quantity_str == 'hetero':
                     quantity = experiment.get_nonlocal_hetero_averaged(complete_im_sets, r, num_theta_bins=theta_bins,
                                                                 skip_grouping=True, calculate_overlap=True)
-                elif quantity == 'Fij_sym':
+                elif quantity_str == 'Fij_sym':
                     quantity = experiment.get_nonlocal_Fij_sym_averaged(complete_im_sets, i, j, r, num_theta_bins=theta_bins,
                                                                 skip_grouping=True, calculate_overlap=True)
-                elif quantity == 'Ftot':
+                elif quantity_str == 'Ftot':
                     quantity = experiment.get_nonlocal_Ftot_averaged(complete_im_sets, r, num_theta_bins=theta_bins,
                                                                 skip_grouping=True, calculate_overlap=True)
                 quantity_list.append(quantity)
             quantity_info['quantity_list'] = quantity_list
-            with open(experiment.title + '_' + quantity + '.pkl', 'wb') as fi:
+            with open(experiment.title + '_' + quantity_str + '.pkl', 'wb') as fi:
                 pkl.dump(quantity_info, fi)
 
     def write_annih_coal_to_disk(self, **kwargs):
