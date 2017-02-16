11d8a27d7890516fdcbfcf35e6ae48deced84a94
bryan <btweinstein@gmail.com>
2017-02-16 18:52:41 -0500

    Added sanity check

--- range_expansion_experiment/image_analysis.py
+++ range_expansion_experiment/image_analysis.py
@@ -1478,18 +1478,18 @@ class Image_Set(object):
 
     def get_max_radius(self):
         cur_brightfield_mask = self.brightfield_mask
-
-        if len(cur_brightfield_mask.shape) > 1: # We measured the radius multiple times
+        if len(cur_brightfield_mask.shape) == 3: # We measured the radius multiple times
             diameter_list = []
             for i in range(cur_brightfield_mask.shape[0]):
                 cur_image = cur_brightfield_mask[i, :, :]
                 # Find maximum diameter
                 r, c = np.where(cur_image)
-                diameter_r = np.float(r.max() - r.min())
-                diameter_c = np.float(c.max() - c.min())
-                # We need both of these checks in case the expansion is bigger than the image in one dimension...
-                desired_diameter = max(diameter_r, diameter_c)
-                diameter_list.append(desired_diameter)
+                if r.shape[0] != 0:
+                    diameter_r = np.float(r.max() - r.min())
+                    diameter_c = np.float(c.max() - c.min())
+                    # We need both of these checks in case the expansion is bigger than the image in one dimension...
+                    desired_diameter = max(diameter_r, diameter_c)
+                    diameter_list.append(desired_diameter)
             diameter_list = np.array(diameter_list)
             # Now find the mean radius
             max_radius = int(np.floor(diameter_list.mean()/2))
@@ -1501,7 +1501,7 @@ class Image_Set(object):
             diameter_r = np.float(r.max() - r.min())
             diameter_c = np.float(c.max() - c.min())
             # We need both of these checks in case the expansion is bigger than the image in one dimension...
-            max_radius = max(diameter_r, diameter_c)
+            max_radius = max(diameter_r, diameter_c)/2.
 
         return max_radius
 
